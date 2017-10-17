f2904f4e2982d4935a11e320a02b283d7d10d80e
Mirtau <tjj72@comcast.net>
2017-10-17 15:56:41 -0600

    finished

--- pixelart.js
+++ pixelart.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
-  for (var i = 0; i < 1026; i++) {
+    var clickedColor = "purple";
+    for (var i = 0; i < 1026; i++) {
     let box = document.getElementsByClassName('grid')[0];
     let tile = document.createElement('div');
 
@@ -11,23 +12,34 @@ document.addEventListener("DOMContentLoaded", function() {
     box.appendChild(tile);
 
   }
+  var psel = ["red", "blue", "green", "yellow", "orange", "pink"];
+
   for (var i = 0; i < 6; i++) {
     let box = document.getElementsByClassName('colors')[0];
-    let tile = document.createElement('div');
+    let palette = document.createElement('div');
 
-    tile.style.border = "1px solid black";
-    tile.style.width = '20px';
-    tile.style.height = '20px';
-    tile.style.float = 'left';
-    tile.className = 'tile';
-    box.appendChild(tile);
+    palette.style.border = "1px solid black";
+    palette.style.width = '20px';
+    palette.style.height = '20px';
+    palette.style.float = 'left';
+    palette.className = 'palette';
+    palette.style.backgroundColor = psel[i];
+    box.appendChild(palette);
+
   }
+  let plate = document.getElementsByClassName('palette')
+  for (let picker of plate){
+   picker.addEventListener('click', function(event) {
+   clickedColor = event.target.style.backgroundColor;
+   console.log(event.target);
+ })
+}
   let grid = document.getElementsByClassName('tile');
 
   for (let pixel of grid) {
     pixel.addEventListener('click', function() {
-      pixel.style.backgroundColor = 'black';
-      console.log('event');
+      pixel.style.backgroundColor = clickedColor;
+
     })
   }
 
